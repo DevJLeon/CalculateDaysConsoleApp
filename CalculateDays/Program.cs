aa5e4108e319d988baf140ad3fcbf68c4d8dc11c
Joshua <celisjoshleon@gmail.com>
2023-12-05 09:01:04 -0500

    feat: :tada: first commit

---
+++ CalculateDays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input Sam daily exercises: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input Kelly daily exercises: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input difference: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Needed days: ");
+        Console.WriteLine(CalculateDays(a,b,c));
+    }
+    public static int CalculateDays(int a, int b, int c)
+{
+    int p1 = 0 + c;
+    int p2 = 0;
+    int days = 0;
+
+    for (int i = 1; p1 + 1 > p2; i++)
+    {
+        p1 += a;
+        p2 += b;
+        days = i;
+    }
+
+    return days;
+}
+
+}
